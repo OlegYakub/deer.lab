4f15cdb27f5ba71455f7c600980add1873bb7bfb
OlegYakub <zeiro299@mail.com>
2017-03-16 18:08:32 +0200

    add banner

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,30 @@
 
 /*==============slick=============*/
 
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  dots: true,
-  infinite: true,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-$(document).ready(function() {
 
 
 
 /*================modal=====================*/
-	
-	$('.btn').click(function(){
-		$('.header').modal({
-		  fadeDuration: 250,
-		  fadeDelay: 0.80
+
+$(document).ready(function(){
+
+$('.rhombus__big').height($('.rhombus__big').width())
+
+$(window).resize(function(){
+	$('.rhombus__big').height($('.rhombus__big').width())
+})
+
+$(window).scroll(function(){
+		var st = $(this).scrollTop();
+
+		console.log(12);
+		$('.left-sq').css({
+		 		"transform" : "translatex( -" + st /70 + "%) rotate(45deg)"
 		});
-	})
-	
+
+		$('.right-sq').css({
+		 		"transform" : "translatex( " + st /70 + "%) rotate(45deg)"
+		});
+});
+
 });
